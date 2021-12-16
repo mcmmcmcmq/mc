2b73bafaaf24e9539dcaccf2dcc9cc26c6e774de
xiaoq <xiaoq@gzhuaxi.com.cn>
2021-12-16 15:35:17 +0800

    first commit

---
+++ configure.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+node_install_path="/opt/node-v12.16.1-linux-x64/"
+
+echo "Start to install MCSManager..."
+echo ""
+mkdir -p ${node_install_path}
+cd ${node_install_path}
+sleep 3
+
+# node
+wget https://npm.taobao.org/mirrors/node/v12.16.1/node-v12.16.1-linux-x64.tar.gz
+
+# Unpack
+echo "Unpacking..."
+echo "> tar -zxf node-v12.16.1-linux-x64.tar.gz"
+tar -zxf node-v12.16.1-linux-x64.tar.gz
+rm -rf node-v12.16.1-linux-x64.tar.gz
+echo "complete."
+
+sleep 1
+
+echo "Linking..."
+echo "> ln -s ${node_install_path}/node-v12.16.1-linux-x64/bin/node /usr/bin/node"
+echo "> ln -s ${node_install_path}/node-v12.16.1-linux-x64/bin/node /usr/bin/node"
+rm -rf /usr/bin/node /usr/bin/npm
+ln -s ${node_install_path}/node-v12.16.1-linux-x64/bin/node /usr/bin/node
+ln -s ${node_install_path}/node-v12.16.1-linux-x64/bin/npm /usr/bin/npm
+echo "complete."
+
+sleep 1
+
+echo "--------------- Node Version ---------------"
+node_version=`node -v`
+npm_version=`npm -v`
+echo " node: ${node_version}"
+echo " npm: ${npm_version}"
+echo "--------------- Node Version ---------------"
+
+sleep 3
+cd ..
+
+echo "Installing git...";
+yum install -y git
+apt install -y git
+pacman -Syu --noconfirm git
+echo "complete."
+
+
+echo "Download MCSManager...";
+# Use Gitee
+
+cd MCSManager
+echo "complete."
+
+sleep 3
+
+echo "Start to install dependent libraries..."
+
+echo "--------------- Complete ---------------"
+echo " Successfully installed!!!"
+echo " Directory: /opt/MCSManager/"
+echo "--------------- Complete ---------------"
+sed -i -e 's/$PORT/'"$PORT"'/g' /opt/MCSManager/model/init_config/property.js
+screen -R mc /usr/bin/node /opt/MCSManager/app.js
