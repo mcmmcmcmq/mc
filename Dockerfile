6c1b7ec67d10ae55ca0d05ae7376de3d48913d5f
xiaoq <xiaoq@gzhuaxi.com.cn>
2021-12-20 15:53:50 +0800

    add auto bak

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,17 @@ RUN apt update -y \
 	&& apt upgrade -y \
  	&& apt install -y vim screen wget curl openjdk-8-jre-headless make python build-essential\
 	&& chmod +x /configure.sh 
-RUN curl -s https://install.zerotier.com | bash
-RUN zerotier-one -d
-RUN zerotier-cli join 8bd5124fd6bd3e02
+ARG PACKAGE_BASEURL=https://download.zerotier.com/debian/buster/pool/main/z/zerotier-one/
+ARG ARCH=amd64
+ARG VERSION=1.8.4
+RUN curl -sSL -o zerotier-one.deb "${PACKAGE_BASEURL}/zerotier-one_${VERSION}_${ARCH}.deb"
+COPY --from=stage zerotier-one.deb .
+
+RUN dpkg -i zerotier-one.deb && rm -f zerotier-one.deb
+RUN echo "${VERSION}" >/etc/zerotier-version
+RUN rm -rf /var/lib/zerotier-one
+COPY start.sh /start.sh
+RUN chmod 755 /start.sh
+
 ENV LANG C.UTF-8
-CMD /configure.sh
+CMD /start.sh
---
+++ start.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+export http_proxy="127.0.0.1:871"
+export https_proxy=$http_proxy
+# export socks5_proxy="socks5://127.0.0.1:870"
+echo "代理设置完毕"
+node_install_path="/opt/node-v12.16.1-linux-x64/"
+
+# 安装rclone
+installRclone() {
+    cd ~
+    echo "正在安装rclone..."
+    wget https://downloads.rclone.org/v1.57.0/rclone-v1.57.0-linux-amd64.zip
+    unzip rclone-v1.57.0-linux-amd64.zip
+    mv rclone-v1.57.0-linux-amd64 rclone
+    cd rclone
+    chmod +x ./rclone
+    mv ./rclone /usr/sbin/
+
+    echo "[mcserver]
+type = dropbox
+token = ${RcloneToken}" >> ~/.config/rclone/rclone.conf
+}
+# 安装环境
+installNode() {
+    mkdir -p ${node_install_path}
+    cd ${node_install_path}
+    sleep 3
+
+    # node
+    wget https://npm.taobao.org/mirrors/node/v12.16.1/node-v12.16.1-linux-x64.tar.gz
+
+    # Unpack
+    echo "Unpacking..."
+    echo "> tar -zxf node-v12.16.1-linux-x64.tar.gz"
+    tar -zxf node-v12.16.1-linux-x64.tar.gz
+    rm -rf node-v12.16.1-linux-x64.tar.gz
+    echo "complete."
+
+    sleep 1
+
+    echo "Linking..."
+    echo "> ln -s ${node_install_path}/node-v12.16.1-linux-x64/bin/node /usr/bin/node"
+    echo "> ln -s ${node_install_path}/node-v12.16.1-linux-x64/bin/node /usr/bin/node"
+    rm -rf /usr/bin/node /usr/bin/npm
+    ln -s ${node_install_path}/node-v12.16.1-linux-x64/bin/node /usr/bin/node
+    ln -s ${node_install_path}/node-v12.16.1-linux-x64/bin/npm /usr/bin/npm
+    echo "complete."
+}
+#安装服务端
+installMCSmanager() {
+    cd ~
+    echo "正在下载管理界面"
+    git clone https://github.com/xhuanya/heroku-mcmanager.git "AllCode"
+    mv ./AllCode/Manager ./Manager
+    rm -rf ./AllCode
+}
+# 检查服务端是否存在
+checkIsInstall() {
+    checkServerHas=$(rclone ls mcserver:/ --cache-db-purge)
+    if [[ "${checkServerHas}" == *"mcserver/"* ]]; then
+        echo "存在"
+          rclone copy mcserver:/mcserver ~/Manager/
+    else
+        echo "不存在"
+        installMCSmanager
+    fi
+}
+start() {
+    cd ~/Manager/
+    npm install
+
+    nohup npm start >./manager.log 2>&1 &
+}
+
+autoBak() {
+    while [ 1==1 ]; do
+        sleep 1h
+        rclone copy  ~/Manager/  mcserver:/mcserver
+        echo "备份完成"
+    done
+}
+
+# 安装
+installNode
+installRclone
+checkIsInstall
+autoBak
